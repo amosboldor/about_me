4ddb7a6d06d494c8711658d85358f669cbb843c9
Amos Boldor <amosboldor@gmail.com>
2016-09-20 14:26:37 -0700

    added the console.log and alert after each question prompt

--- app.js
+++ app.js
@@ -10,12 +10,24 @@ console.log('The user will ride a ' + ride + ' into battle');
 alert('Epic music is playing in the background then you turn around and see in the distance ' + name + ' riding a ' + ride + ' towards a sea of angry spiders while wielding a ' + lighSaberColor + ' lightsaber and screaming a the top of ' + gender + ' lungs.');
 alert('THE END')*/
 
-var questionOne = prompt('Do I have hazelnut eyes?').toLowerCase();
+var userName = prompt('What is your first name?');
 
-var questionTwo = prompt('Do I love Seattle').toLowerCase();
+var questionOne = prompt('Do I have hazelnut eyes?').toLowerCase();
+console.log('The user answered ' + questionOne + ' for do I have hazelnut eyes?')
+alert('Yes, My eyes are hazelnut!');
+
+var questionTwo = prompt('Do I love Seattle?').toLowerCase();
+console.log('The user answered ' + questionTwo + ' for Do I love Seattle?');
+alert('Yes, I love Seattle!');
 
 var questionThree = prompt('Do I enjoy kale?').toLowerCase();
+console.log('The user answered ' + questionThree + ' for do I enjoy kale?');
+alert('No, I hate kale?');
 
-var questionFour = prompt('Do I like the TV show Rick and Morty?').toLowerCase();
+var questionFour = prompt('Do I love the TV show Rick and Morty?').toLowerCase();
+console.log('The user answered ' + questionFour + ' for do I love the TV show Rick and Morty?');
+alert('Yes, I love Rick and Morty');
 
 var questionFive = prompt('Do I plan on visiting Somalia?').toLowerCase();
+console.log('The user answered ' + questionFive + ' for do I plan on visiting Somalia?');
+alert('No, unless they have Starbucks');
