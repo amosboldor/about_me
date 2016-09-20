f41f97b97b6da2ab52fa3bddc350c41640c720bb
Amos Boldor <amosboldor@gmail.com>
2016-09-20 13:50:29 -0700

    added 5 prompt questions

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
-var name = prompt('What is your name?');
+"use strict";
+/* var name = prompt('What is your name?');
 console.log('The user\'s name is ' + name);
 var gender = prompt('Would you consider ' + name + ' a his or a her?');
 console.log('The user chose ' + gender);
@@ -7,4 +8,14 @@ console.log('The user\'s favorite color is ' + lighSaberColor)
 var ride = prompt('What do you like most a bunny a dragon or a horse?');
 console.log('The user will ride a ' + ride + ' into battle');
 alert('Epic music is playing in the background then you turn around and see in the distance ' + name + ' riding a ' + ride + ' towards a sea of angry spiders while wielding a ' + lighSaberColor + ' lightsaber and screaming a the top of ' + gender + ' lungs.');
-alert('THE END')
+alert('THE END')*/
+
+var questionOne = prompt('Do I have hazelnut eyes?').toLowerCase();
+
+var questionTwo = prompt('Do I love Seattle').toLowerCase();
+
+var questionThree = prompt('Do I enjoy kale?').toLowerCase();
+
+var questionFour = prompt('Do I like the TV show Rick and Morty?').toLowerCase();
+
+var questionFive = prompt('Do I plan on visiting Somalia?').toLowerCase();
