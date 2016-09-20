49e2c0003a09353d9bcac72144f35bf966830331
Amos Boldor <amosboldor@gmail.com>
2016-09-20 15:48:40 -0700

    I deleted the first day lab code and replaced the alert/console.log with more responsive if statments

--- app.js
+++ app.js
@@ -1,33 +1,63 @@
 "use strict";
-/* var name = prompt('What is your name?');
-console.log('The user\'s name is ' + name);
-var gender = prompt('Would you consider ' + name + ' a his or a her?');
-console.log('The user chose ' + gender);
-var lighSaberColor = prompt('What is your favorite color?');
-console.log('The user\'s favorite color is ' + lighSaberColor)
-var ride = prompt('What do you like most a bunny a dragon or a horse?');
-console.log('The user will ride a ' + ride + ' into battle');
-alert('Epic music is playing in the background then you turn around and see in the distance ' + name + ' riding a ' + ride + ' towards a sea of angry spiders while wielding a ' + lighSaberColor + ' lightsaber and screaming a the top of ' + gender + ' lungs.');
-alert('THE END')*/
 
 var userName = prompt('What is your first name?');
+console.log('The user\'s name is: ' + userName);
+alert('Hello ' + userName + ' and welcome to my about me webpage but first here is a quick game to ge to know me.');
 
 var questionOne = prompt('Do I have hazelnut eyes?').toLowerCase();
-console.log('The user answered ' + questionOne + ' for do I have hazelnut eyes?')
-alert('Yes, My eyes are hazelnut!');
+if (questionOne === 'yes') {
+  alert('Yes ' + userName + ' you got it right!');
+  console.log(userName + ' got the do I have hazelnut eyes question [right].');
+} else if (questionOne === 'no') {
+  alert('Sorry ' + userName + ' you got it wrong!');
+  console.log(userName + ' got the do I have hazelnut eyes question [wrong].');
+} else {
+  alert('Wrong input refresh and try again!');
+  console.log('Wrong input refresh and try again!');
+}
+
 
 var questionTwo = prompt('Do I love Seattle?').toLowerCase();
-console.log('The user answered ' + questionTwo + ' for Do I love Seattle?');
-alert('Yes, I love Seattle!');
+if (questionTwo === 'yes') {
+  alert('Yes ' + userName + ' how did you know!');
+  console.log(userName + ' got the do I love Seattle question [right].');
+} else if (questionTwo === 'no') {
+  alert('Sorry ' + userName + ' you got it wrong, Seattle is AWESOME!');
+  console.log(userName + ' got the do I love Seattle question [wrong].');
+} else {
+  alert('Wrong input refresh and try again!');
+  console.log('Wrong input refresh and try again');
+}
 
 var questionThree = prompt('Do I enjoy kale?').toLowerCase();
-console.log('The user answered ' + questionThree + ' for do I enjoy kale?');
-alert('No, I hate kale?');
+if (questionThree === 'yes') {
+  alert('No ' + ' I do not enjoy kale!');
+  console.log(userName + 'got the do I enjoy kale question [wrong].');
+} else if (questionThree === 'no') {
+  alert('Yes ' + userName + ' you got it [right]. \n KALE SUCKS');
+  console.log(userName + ' got the do I enjoy kale question [right].');
+} else {
+  alert('Wrong input refresh and try again!');
+  console.log('Wrong input refresh and try again!');
+}
 
 var questionFour = prompt('Do I love the TV show Rick and Morty?').toLowerCase();
-console.log('The user answered ' + questionFour + ' for do I love the TV show Rick and Morty?');
-alert('Yes, I love Rick and Morty');
+if (questionFour === 'yes') {
+  alert('Yes ' + userName + ' you got it right\n HAIL RICK AND MORTY');
+  console.log(userName + ' got the question do I love the TV show Rick and Morty [right].');
+} else if (questionFour === 'no') {
+  alert('No ' + userName + ' you are so wrong!');
+  console.log('The user ' + userName + ' got the question do I love the TV show Rick and Morty [wrong].');
+}
 
 var questionFive = prompt('Do I plan on visiting Somalia?').toLowerCase();
-console.log('The user answered ' + questionFive + ' for do I plan on visiting Somalia?');
-alert('No, unless they have Starbucks');
+if (questionFive === 'yes') {
+  alert('No ' + userName + ' you were wrong, unless they get a Starbucks');
+  console.log(userName + ' got the Do I plan on visiting Somalia question [wrong].')
+} else if (questionFive === 'no') {
+  alert('Yes ' + userName + ' you got it right.')
+  console.log(userName + ' got the Do I plan on visiting Somalia question [right].')
+} else {
+  alert('Wrong input refresh and try again!');
+  console.log('Wrong input refresh and try again');
+}
