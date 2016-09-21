d99ab27ea2c4aa43b9acfd0eb9d66aabcb480bcd
Amos Boldor <amosboldor@gmail.com>
2016-09-20 20:53:16 -0700

    put in the finishing touches

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var userName = prompt('What is your first name?');
 console.log('The user\'s name is: ' + userName);
 alert('Hello ' + userName + ' and welcome to my about me webpage but first here is a quick game to ge to know me.');
 
-var questionOne = prompt('Do I have hazelnut eyes?').toLowerCase();
+var questionOne = prompt('Do I have hazelnut eyes? (yes or no)').toLowerCase();
 if (questionOne === 'yes') {
   alert('Yes ' + userName + ' you got it right!');
   console.log(userName + ' got the do I have hazelnut eyes question [right].');
@@ -17,7 +17,7 @@ if (questionOne === 'yes') {
 }
 
 
-var questionTwo = prompt('Do I love Seattle?').toLowerCase();
+var questionTwo = prompt('Do I love Seattle? (yes or no)').toLowerCase();
 if (questionTwo === 'yes') {
   alert('Yes ' + userName + ' how did you know!');
   console.log(userName + ' got the do I love Seattle question [right].');
@@ -29,7 +29,7 @@ if (questionTwo === 'yes') {
   console.log('Wrong input refresh and try again');
 }
 
-var questionThree = prompt('Do I enjoy kale?').toLowerCase();
+var questionThree = prompt('Do I enjoy kale? (yes or no)').toLowerCase();
 if (questionThree === 'yes') {
   alert('No ' + ' I do not enjoy kale!');
   console.log(userName + 'got the do I enjoy kale question [wrong].');
@@ -41,7 +41,7 @@ if (questionThree === 'yes') {
   console.log('Wrong input refresh and try again!');
 }
 
-var questionFour = prompt('Do I love the TV show Rick and Morty?').toLowerCase();
+var questionFour = prompt('Do I love the TV show Rick and Morty? (yes or no)').toLowerCase();
 if (questionFour === 'yes') {
   alert('Yes ' + userName + ' you got it right\n HAIL RICK AND MORTY');
   console.log(userName + ' got the question do I love the TV show Rick and Morty [right].');
@@ -50,7 +50,7 @@ if (questionFour === 'yes') {
   console.log('The user ' + userName + ' got the question do I love the TV show Rick and Morty [wrong].');
 }
 
-var questionFive = prompt('Do I plan on visiting Somalia?').toLowerCase();
+var questionFive = prompt('Do I plan on visiting Somalia? (yes or no)').toLowerCase();
 if (questionFive === 'yes') {
   alert('No ' + userName + ' you were wrong, unless they get a Starbucks');
   console.log(userName + ' got the Do I plan on visiting Somalia question [wrong].')
